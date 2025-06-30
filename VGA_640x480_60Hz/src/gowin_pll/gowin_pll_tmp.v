95c6cd085c47e0729939a3b52e124499a9a79f9d
MatrixMKII <erik.myrva@gmail.com>
2025-06-30 22:20:45 +0200

    Add files via upload

---
+++ VGA_640x480_60Hz/src/gowin_pll/gowin_pll_tmp.v
@@ -0,0 +1,18 @@
+//Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.11.01 Education (64-bit)
+//Part Number: GW5A-LV25MG121NC1/I0
+//Device: GW5A-25
+//Device Version: A
+//Created Time: Mon Jun 30 03:13:18 2025
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_PLL your_instance_name(
+        .clkout0(clkout0), //output clkout0
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
---
+++ VGA_640x480_60Hz/src/gowin_pll/pll_pixelclk_tmp.vhd
@@ -0,0 +1,26 @@
+--Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+--All rights reserved.
+--File Title: Template file for instantiation
+--Tool Version: V1.9.11.01 Education (64-bit)
+--Part Number: GW5A-LV25MG121NC1/I0
+--Device: GW5A-25
+--Device Version: A
+--Created Time: Mon Jun 30 00:59:10 2025
+
+--Change the instance name and port connections to the signal names
+----------Copy here to design--------
+
+component Gowin_PLL
+    port (
+        clkout0: out std_logic;
+        clkin: in std_logic
+    );
+end component;
+
+your_instance_name: Gowin_PLL
+    port map (
+        clkout0 => clkout0,
+        clkin => clkin
+    );
+
+----------Copy end-------------------
